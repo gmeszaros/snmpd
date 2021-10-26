b3f5c230436919ac43b597431ad255d7160d7863
Sreekrishnan P <krishna.u73@gmail.com>
2021-10-26 18:04:24 +0530

    Implemented user creation

--- README.md
+++ README.md
@@ -2,4 +2,34 @@
 
 This subordinate charm will deploy snmpd
 
-Read metadata file for options.
+## Usage
+
+```
+juju deploy snmpd
+juju add-relation ubuntu snmpd
+```
+
+To retrieve the configured pass phrases, run the below commands, 
+```
+juju run-action --wait snmpd/X show-auth-pass-phrase
+juju run-action --wait snmpd/X show-priv-pass-phrase
+```
+
+### Note:-
+1. Pass phrases can be updated either as an option or as include-file in the YAML format.
+
+```
+  snmpv3_pass_phrase:
+    default: |
+      ---
+      auth_pass_phrase: "auth_password"
+      priv_pass_phrase: "priv_password"
+    description: |
+      Define the default authentication and privacy pass phrases to use for SNMPv3 requests.
+      example (using include-file):
+        snmpv3_pass_phrase: include-file://snmpd_pass_phrase.yaml
+```
+
+2. Irrespective of the previous ```security_name```, it creates new user but pass phrases can be updated for the same user.
+    
+    Ex: If you changed the pass phrases for the same user, previous pass phrases never work, but works with the latest supplied pass phrases.
--- reactive/snmpd.py
+++ reactive/snmpd.py
@@ -1,19 +1,28 @@
+import subprocess
+import yaml
 from charmhelpers import fetch
 from charmhelpers.core import host, hookenv, unitdata
 from charmhelpers.core.templating import render
-from charms.reactive import when, when_not, when_any, when_file_changed,when_not_all, remove_state, set_state, hook
+from charms.reactive import (
+    when, when_not, when_any, when_file_changed,
+    set_state, hook)
 
 SNMPD_CONF = '/etc/snmp/snmpd.conf'
 
+
+def get_auth_pass_phrase():
+    db = unitdata.kv()
+    return db.get("priv_protocol")
 
 @when_not('snmpd.installed',
           'snmpd.stopped')
 def install_snmpd():
     install_apt_packages()
+    start_snmpd()
     set_state('snmpd.installed')
 
 
-@when('snmpd.installed')
+@when('config.changed')
 def start_snmpd():
     update_snmpd_conf()
     if not host.service_running('snmpd'):
@@ -25,10 +34,40 @@ def start_snmpd():
     hookenv.status_set('active', 'Ready')
 
 
+@when_any('config.changed.snmpv3_pass_phrase',
+      'config.changed.auth_protocol',
+      'config.changed.priv_protocol',
+      'config.changed.security_name')
+def create_user():
+    config = hookenv.config()
+    yaml_data = yaml.safe_load(config["snmpv3_pass_phrase"])
+    auth_protocol = config['auth_protocol']
+    priv_protocol = config['priv_protocol']
+    security_name = config['security_name']
+    auth_pass_phrase = yaml_data.get('auth_pass_phrase')
+    priv_pass_phrase = yaml_data.get('priv_pass_phrase')
+
+    host.service_stop('snmpd')
+    if not host.service_running('snmpd'):
+        try:
+            cmd = f"net-snmp-create-v3-user -ro " \
+                  f"-A {auth_pass_phrase} " \
+                  f"-a {auth_protocol} " \
+                  f"-X {priv_pass_phrase} " \
+                  f"-x {priv_protocol} " \
+                  f"{security_name}"
+
+            subprocess.check_call(cmd, shell=True)
+        except subprocess.CalledProcessError as e:
+            hookenv.log(f'Failed to create user {security_name}')
+            hookenv.log('Error {}'.format(e), hookenv.ERROR)
+
+    host.service_start('snmpd')
+
 @when('config.changed.extra_packages')
 def install_apt_packages():
     config = hookenv.config()
-    packages = ['snmpd']
+    packages = ['snmpd', 'libsnmp-dev']
     if config['extra_packages']:
         packages += [p.strip() for p in config['extra_packages'].split(',')]
     hookenv.status_set('maintenance', 'Installing packages')
@@ -36,7 +75,7 @@ def install_apt_packages():
     fetch.configure_sources()
     fetch.apt_update()
     fetch.apt_install(packages)
-# TODO: remove unnecessary packages
+    hookenv.status_set('active', 'Ready')
 
 
 @when_file_changed(SNMPD_CONF, hash_type='sha256')
@@ -58,8 +97,7 @@ def update_snmpd_conf():
         owner='root',
         group='root'
     )
-
-    hookenv.status_set('maintenance', 'snmpd.conf updated')
+    hookenv.status_set('active', 'Ready')
 
 
 @when_any('host.available', 'host.connected')
@@ -82,7 +120,7 @@ def remove_snmpd():
 
 def uninstall_packages():
     kv = unitdata.kv()
-    packages = ['snmpd']
+    packages = ['snmpd', 'libsnmp-dev']
     # if kv.get('extra_packages'):
     #     packages += [p.strip() for p in kv.get('extra_packages').split(',')]
     hookenv.log("Uninstalling.. " + str([pkg for pkg in packages]), hookenv.INFO)
